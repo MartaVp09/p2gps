ba885c4aabc1e6a0f3c36961fc89b0190b2f8695
MARTA VALIENTE PARDO <Marta.Valiente1@alu.uclm.es>
2022-11-13 14:44:34 +0100

    Contenedores

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#no lanzar el jar hasta que no esté accesible el servicio de MongoDB 
+#el host 'mongo' tendrá una IP otorgada al lanzar la pila de #servicios,
+#y le damos el apodo 'mongo', utilizado aquí y en el #archivo application.properties
+
 while ! curl http://mongo:27017/
 do
   echo "$(date) - still trying"
   sleep 1
 done
-echo "$(date) - connected successfully"
+echo "$(date) - connected succesfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config. location=application.properties
